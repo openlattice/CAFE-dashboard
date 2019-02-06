b9c3a7494333aa17b08a03dbe7e3166af5a85b11
jokedurnez <joke.durnez@gmail.com>
2019-02-05 20:36:30 -0800

    add Chivo link

--- ui.R
+++ ui.R
@@ -19,6 +19,7 @@ shinyUI(
       # fluid = TRUE,
       header = tags$head(
         tags$style(HTML("#page-nav > li:first-child { display: none; }")),
+        tags$link(rel= "stylesheet", type = "text/css", href = "http://fonts.googleapis.com/css?family=Chivo"),
         tags$link(rel= "stylesheet", type = "text/css", href = "http://fonts.googleapis.com/css?family=Open+Sans"),
         tags$link(rel= "stylesheet", type = "text/css", href = "AdminLTE.css"),
         tags$link(rel= "stylesheet", type = "text/css", href = "shinydashboard.css"),
