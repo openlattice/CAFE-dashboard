d849eca24a36cb8eee012733966eb8f46f070cef
jokedurnez <joke.durnez@gmail.com>
2019-02-20 21:14:03 -0800

    write testdata for efficiency

--- testscript.R
+++ testscript.R
@@ -8,21 +8,12 @@ source("pipelines/tud_summarise_data.R")
 source("servercomponents/plots.R")
 
 jwt <- "*"
+
 rawdata <- load_data(jwt, local = FALSE)
 activitydata <- process_activities(rawdata)
 summarydata <- summarise_data(activitydata)
 
+# activitydata <- read_csv( "/Users/jokedurnez/Desktop/test.csv")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+write_csv(activitydata, "/Users/jokedurnez/Desktop/test.csv")
+write_csv(summarydata, "/Users/jokedurnez/Desktop/test_sum.csv")
