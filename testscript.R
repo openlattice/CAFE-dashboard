2a7db9b01e1cc565b2e16203aaac05620a8a6d87
jokedurnez <joke.durnez@gmail.com>
2019-02-21 17:12:26 -0800

    make testscripts great again

--- testscript.R
+++ testscript.R
@@ -7,13 +7,8 @@ source("pipelines/tud_transform_data.R")
 source("pipelines/tud_summarise_data.R")
 source("servercomponents/plots.R")
 
-jwt <- "*"
+jwt = "*"
 
 rawdata <- load_data(jwt, local = FALSE)
 activitydata <- process_activities(rawdata)
 summarydata <- summarise_data(activitydata)
-
-# activitydata <- read_csv( "/Users/jokedurnez/Desktop/test.csv")
-
-write_csv(activitydata, "/Users/jokedurnez/Desktop/test.csv")
-write_csv(summarydata, "/Users/jokedurnez/Desktop/test_sum.csv")
