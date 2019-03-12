14899bb9714a513df2a5aac377a462c8088522fb
jokedurnez <joke.durnez@gmail.com>
2019-03-11 22:35:19 -0700

    update testscript for grabbing data

--- testscript.R
+++ testscript.R
@@ -1,14 +1,6 @@
-library(tidyverse)
-library(openlattice)
-
 setwd("/Users/jokedurnez/Documents/accounts/CAFE/CAFE_code/dashboard/")
-source("pipelines/tud_load_data.R")
-source("pipelines/tud_transform_data.R")
-source("pipelines/tud_summarise_data.R")
-source("servercomponents/plots.R")
+source("global.R")
 
-jwt = "*"
+jwt <- "*"
 
-rawdata <- load_data(jwt, local = FALSE)
-activitydata <- process_activities(rawdata)
-summarydata <- summarise_data(activitydata)
+rawdata <- get_data(jwt, cache=TRUE, auth=TRUE)
