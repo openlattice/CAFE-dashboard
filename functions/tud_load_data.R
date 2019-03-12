abee399fa323e046d8093357adb950442fc9aaea
jokedurnez <joke.durnez@gmail.com>
2019-03-11 22:33:05 -0700

    add subsetting function and format

--- functions/tud_load_data.R
+++ functions/tud_load_data.R
@@ -1,26 +1,38 @@
-# subset_data <- function(rawdata, hourrange = NULL, agerange = NULL, progressrange = NULL, sites = NULL) {
-#     if (!is.null(hourrange)){
-#         dur_by_session <- rawdata$tud$preprocessed %>%
-#             group_by(day_id) %>%
-#             summarise(duration = sum(duration) / 60) %>%
-#             filter(duration > hourrange[1] & duration < hourrange[2]) %>%
-#             select("day_id")
-#         rawdata[['processed']] <- rawdata$tud$preprocessed %>% filter(day_id %in% as_vector(dur_by_session))
-#     }
-# # if (!is.null(agerange)){
-# # 
-# # }
-# 
-#     return(rawdata)
-# }
+subset_data <-
+    function(rawdata,
+             hourrange = NULL,
+             agerange = NULL,
+             progressrange = NULL,
+             sites = NULL) {
+        output = list(tud = rawdata$tud$preprocessed,
+                      maq = rawdata$maq$preprocessed)
+        if (sum(hourrange) != FALSE) {
+            subset <- output$tud %>%
+                group_by(child_id, day_id) %>%
+                summarise(duration = sum(duration) / 60) %>%
+                filter(duration > hourrange[1] &
+                           duration < hourrange[2])
+            childreninrange <- subset %>% select("child_id")
+            daysinrange <- subset %>% select("day_id")
+            output$tud <- output$tud %>%
+                filter(child_id %in% as_vector(childreninrange)) %>%
+                filter(day_id %in% as_vector(daysinrange))
+            output$maq <- output$tud %>%
+                filter(nc.SubjectIdentification %in% as_vector(childreninrange))
+        }
+        
+        return(output)
+    }
 
 
-get_data <- function(jwt, cache = FALSE, auth=FALSE) {
+get_data <- function(jwt, cache = FALSE, auth = FALSE) {
     print("Getting authenticated !")
     
-    if(auth == FALSE){
+    if (auth == FALSE) {
         apis <- get_apis(jwt)
-        if (is.null(apis)) {return (get_empty_rawdata())}
+        if (is.null(apis)) {
+            return (get_empty_rawdata())
+        }
     } else {
         apis <- NULL
     }
@@ -31,52 +43,40 @@ get_data <- function(jwt, cache = FALSE, auth=FALSE) {
         rawdata <- load_data(apis)
     }
     
-    if (auth == FALSE){
+    if (auth == FALSE) {
         rawdata['auth'] <- is_authorized(apis)
     } else {
         rawdata['auth'] <- TRUE
     }
-
+    
     rawdata$tud[['preprocessed']] = process_activities(rawdata)
     rawdata$tud[['processed']] = process_activities(rawdata)
     rawdata$tud[['summarised']] = summarise_data(rawdata$tud[['processed']])
     rawdata$maq[['preprocessed']] = process_maq(rawdata)
     rawdata$maq[['processed']] = process_maq(rawdata)
- 
+    
     rawdata$tud[['processed_coltypes']] <- list(
-                numeric = rawdata$tud$processed %>% select(which(sapply(
-                    ., is.numeric
-                ))) %>% names,
-                factorial = rawdata$tud$processed %>% select(which(sapply(
-                    ., is.factor
-                ))) %>% names,
-                boolean = rawdata$tud$processed %>% select(which(sapply(
-                    ., is.logical
-                ))) %>% names
-            )
-
+        numeric = rawdata$tud$processed %>% select(which(sapply(., is.numeric))) %>% names,
+        factorial = rawdata$tud$processed %>% select(which(sapply(., is.factor))) %>% names,
+        boolean = rawdata$tud$processed %>% select(which(sapply(., is.logical))) %>% names
+    )
+    
     rawdata$tud[['summarised_coltypes']] <- list(
-        numeric = rawdata$tud$summarised %>% select(which(sapply(
-                    ., is.numeric
-                ))) %>% names,
-                factorial = rawdata$tud$summarised %>% select(which(sapply(
-                    ., is.factor
-                ))) %>% names,
-                boolean = rawdata$tud$summarised %>% select(which(sapply(
-                    ., is.logical
-                ))) %>% names
-            )
-
+        numeric = rawdata$tud$summarised %>% select(which(sapply(., is.numeric))) %>% names,
+        factorial = rawdata$tud$summarised %>% select(which(sapply(., is.factor))) %>% names,
+        boolean = rawdata$tud$summarised %>% select(which(sapply(., is.logical))) %>% names
+    )
+    
     
     return(rawdata)
     
 }
 
 
-read_data <- function(apis,auth = FALSE) {
+read_data <- function(apis, auth = FALSE) {
     print("Getting the data !")
     rawdata <- read_yaml("data/rawdata.yaml")
-    rawdata <- add_authentication_to_raw(rawdata, apis, auth=auth)
+    rawdata <- add_authentication_to_raw(rawdata, apis, auth = auth)
     print("Got the data !")
     return(rawdata)
 }
@@ -89,39 +89,36 @@ load_data <-
         # print("-- TUD: Getting nodes.")
         # datasets <- TUD_entities %>% map(get_node_table, apis)
         # names(datasets) <- TUD_entities
-        # 
+        #
         # print("-- TUD: Getting edges.")
         # edgesdata <- TUD_associations %>% map(get_edge_table, datasets, apis)
         # names(edgesdata) <-
         #     TUD_associations %>% map_chr(function(x) {
         #         return (paste0(x['src'], "_", x['dst']))
         #     })
-        # 
+        #
         # MAQ
         print("-- MAQ: Getting nodes.")
         maqdatasets <- MAQ_entities %>% map(get_node_table, apis)
         names(maqdatasets) <- MAQ_entities
         
         print("-- MAQ: Getting edges.")
-        maqedgesdata <- MAQ_associations %>% map(get_edge_table, maqdatasets, apis)
+        maqedgesdata <-
+            MAQ_associations %>% map(get_edge_table, maqdatasets, apis)
         names(maqedgesdata) <-
             MAQ_associations %>% map_chr(function(x) {
                 return (paste0(x['src'], "_", x['dst']))
             })
-
+        
         outdata <- list(
             # tud = list(
             #     nodes = datasets,
             #     edges = edgesdata
             # ),
-            chronicle = list(
-                raw = tibble(),
-                processed = tibble()
-            ),
-            maq = list(
-                nodes = maqdatasets,
-                edges = maqedgesdata
-            ),
+            chronicle = list(raw = tibble(),
+                             processed = tibble()),
+            maq = list(nodes = maqdatasets,
+                       edges = maqedgesdata),
             n_act = dim(datasets$primary_activity)[1],
             n_child = dim(datasets$people)[1],
             auth = TRUE
@@ -131,30 +128,24 @@ load_data <-
         return (outdata)
     }
 
-get_empty_rawdata <- function(){
+get_empty_rawdata <- function() {
     return (list(
-        tud = list(
-            nodes = list(),
-            edges = list()
-        ),
-        chronicle = list(
-            raw = tibble(),
-            processed = tibble()
-        ),
+        tud = list(nodes = list(),
+                   edges = list()),
+        chronicle = list(raw = tibble(),
+                         processed = tibble()),
         auth = FALSE,
         n_act = 0,
         n_child = 0
     ))
 }
 
-get_auth <- function(study, entitysets){
+get_auth <- function(study, entitysets) {
     return(ifelse(study %in% entitysets, TRUE, FALSE))
 }
 
 
-add_authentication_to_raw <- function(data, apis, auth = FALSE){
-    
-    
+add_authentication_to_raw <- function(data, apis, auth = FALSE) {
     newdata <- list(
         tud = list(edges = list(), nodes = list()),
         chronicle = list(raw = list(), processed = list()),
@@ -163,7 +154,7 @@ add_authentication_to_raw <- function(data, apis, auth = FALSE){
         n_act = data$n_act
     )
     
-    if (auth == FALSE){
+    if (auth == FALSE) {
         entitysets <- apis$personal$edmApi$get_all_entity_sets()$name
     }
     
@@ -174,16 +165,17 @@ add_authentication_to_raw <- function(data, apis, auth = FALSE){
     
     for (node in names(data$tud$nodes)) {
         nodetable <- as.tibble(data$tud$nodes[[node]])
-        if (dim(nodetable)[1]==0){
+        if (dim(nodetable)[1] == 0) {
             nodetable = nodetable
-        } else if (auth==FALSE){
-            nodetable <- nodetable %>% mutate(table_access = study %in% entitysets)
+        } else if (auth == FALSE) {
+            nodetable <-
+                nodetable %>% mutate(table_access = study %in% entitysets)
         } else {
             nodetable <- nodetable %>% mutate(table_access = TRUE)
         }
         newdata$tud$nodes[[node]] <- nodetable
     }
-        
+    
     # MAQ
     for (edge in names(data$maq$edges)) {
         newdata$maq$edges[[edge]] <- as.tibble(data$maq$edges[[edge]])
@@ -191,10 +183,11 @@ add_authentication_to_raw <- function(data, apis, auth = FALSE){
     
     for (node in names(data$maq$nodes)) {
         maqnodetable <- as.tibble(data$maq$nodes[[node]])
-        if (dim(maqnodetable)[1]==0){
+        if (dim(maqnodetable)[1] == 0) {
             maqnodetable <- maqnodetable
-        } else if (auth==FALSE ){
-            maqnodetable <- maqnodetable %>% mutate(table_access = study %in% entitysets)
+        } else if (auth == FALSE) {
+            maqnodetable <-
+                maqnodetable %>% mutate(table_access = study %in% entitysets)
         } else {
             maqnodetable <- maqnodetable %>% mutate(table_access = TRUE)
         }
@@ -203,22 +196,23 @@ add_authentication_to_raw <- function(data, apis, auth = FALSE){
     
     # chronicle
     rawtable <- as.tibble(data$chronicle$raw)
-    if (auth==FALSE){
-        rawtable <- rawtable %>% mutate(table_access = study %in% entitysets)
+    if (auth == FALSE) {
+        rawtable <-
+            rawtable %>% mutate(table_access = study %in% entitysets)
     } else {
         rawtable <- rawtable %>% mutate(table_access = TRUE)
     }
     
     proctable <- as.tibble(data$chronicle$processed)
-    if (auth==FALSE){
-        proctable <- proctable %>% mutate(table_access = study %in% entitysets)
+    if (auth == FALSE) {
+        proctable <-
+            proctable %>% mutate(table_access = study %in% entitysets)
     } else {
         proctable <- proctable %>% mutate(table_access = TRUE)
     }
     newdata$chronicle$raw = rawtable
     newdata$chronicle$processed = proctable
-
+    
     return(newdata)
     
 }
-
