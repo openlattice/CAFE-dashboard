7b3fb3f4d196428400c19956cc3034e1ed1ed19c
jokedurnez <joke.durnez@gmail.com>
2019-04-22 17:18:44 -0700

    update

--- testscript.R
+++ testscript.R
@@ -2,7 +2,6 @@ setwd("/Users/jokedurnez/Documents/accounts/CAFE/CAFE_code/dashboard/")
 source("global.R")
 
 # read in for local testing
-jwt="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Impva2VAb3BlbmxhdHRpY2UuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInVzZXJfbWV0YWRhdGEiOnt9LCJhcHBfbWV0YWRhdGEiOnsicm9sZXMiOlsiQXV0aGVudGljYXRlZFVzZXIiLCJhZG1pbiJdLCJvcmdhbml6YXRpb25zIjpbIjAwMDAwMDAwLTAwMDAtMDAwMS0wMDAwLTAwMDAwMDAwMDAwMCIsImU2NmM5ZWVmLTIzNzktNGEwYy05Y2JiLWM2ZDhiMmIxM2M1ZiJdfSwibmlja25hbWUiOiJqb2tlIiwicm9sZXMiOlsiQXV0aGVudGljYXRlZFVzZXIiLCJhZG1pbiJdLCJ1c2VyX2lkIjoiZ29vZ2xlLW9hdXRoMnwxMDM3NzE3NTU1NDA4NjUyOTkyNDgiLCJpc3MiOiJodHRwczovL29wZW5sYXR0aWNlLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwMzc3MTc1NTU0MDg2NTI5OTI0OCIsImF1ZCI6IktUemd5eHM2S0JjSkhCODcyZVNNZTJjcFRIemh4Uzk5IiwiaWF0IjoxNTU1NjkxNDE2LCJleHAiOjE1NTU3Nzc4MTZ9.cwnQ2XtFubG1UQxMc0y5QI6aaqFZijnvyQGveAJ11E8"
 local = TRUE
 auth = TRUE
 cache = TRUE
@@ -17,7 +16,9 @@ subset_tosave <- list(
     maq = list(nodes = rawdata$maq$nodes, edges = rawdata$maq$edges),
     chronicle = list(raw = chr$chronicle$raw)
 )
-write_yaml(subset_tosave, "data/rawdata_20190418.yaml")
+write_yaml(subset_tosave, "data/rawdata_20190422.yaml")
 
 
 
+apis <- get_apis(jwt, TRUE)
+rawdata <- load_data(apis, TUD=FALSE)
