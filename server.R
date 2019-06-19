fb9de9e67d9498f019fa6273584026af1f8ed001
jokedurnez <joke.durnez@gmail.com>
2019-06-19 13:29:00 -0700

    add rawdata as argument to documentation

--- server.R
+++ server.R
@@ -214,6 +214,6 @@ shinyServer(function(input, output, session) {
     callModule(multivariate_server, "analysis", rawdata)
     callModule(multivariate_cor_server, "analysis", rawdata)
     callModule(module = esquisserServer, id = "esquisse", data = data_r)
-    callModule(documentation_server, "documentation")
+    callModule(documentation_server, "documentation", rawdata)
     
 })
